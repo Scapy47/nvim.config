28c29920ca26b57e614745eafd256f0f99b324a6
scapy_47 <134828740+Scapy47@users.noreply.github.com>
2025-03-30 17:12:54 +0530

    added PlatformIO wrapper for neovim nvim-platformio.lua

--- lua/plugins/00-List.lua
+++ lua/plugins/00-List.lua
@@ -1,12 +1,34 @@
 return {
+    -- PERF: Typing prectice
     {
         "nvzone/typr",
         dependencies = "nvzone/volt",
         opts = {},
         cmd = { "Typr", "TyprStats" },
     },
+    -- PERF: cursor animation
     {
         "sphamba/smear-cursor.nvim",
+        event = 'VimEnter',
         opts = {},
+    },
+    -- PERF: platformio
+    {
+        "anurag3301/nvim-platformio.lua",
+        dependencies = {
+            { "akinsho/nvim-toggleterm.lua" },
+            { "nvim-telescope/telescope.nvim" },
+            { "nvim-lua/plenary.nvim" },
+        },
+        cmd = {
+            "Pioinit",
+            "Piorun",
+            "Piocmd",
+            "Piolib",
+            "Piomon",
+            "Piodebug",
+            "Piodb",
+        },
+        opts = {}
     }
 }
