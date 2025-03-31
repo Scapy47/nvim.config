224d939a597e3b59c27835e6bab2f16971db36fd
scapy_47 <134828740+Scapy47@users.noreply.github.com>
2025-03-31 12:41:44 +0530

    ....

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -3,6 +3,7 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
         options = {
+            icons_enabled = vim.g.have_nerd_font,
             section_separators = { left = '', right = '' },
             component_separators = { left = '', right = '' }
         }
